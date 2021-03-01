05d51a0b0e3c1cf7a43ad49a450f41bf6aba7269
juliusryan25 <63332117+juliusryan25@users.noreply.github.com>
2021-03-01 16:50:59 +0700

    first_commit

---
+++ android/app/src/main/kotlin/com/example/dw_employee_crud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dw_employee_crud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './pages/employee.dart';
+import 'package:provider/provider.dart';
+import 'package:dw_employee_crud/providers/employee_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => EmployeeProvider(),
+        )
+      ],
+    child: MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(        
+        primarySwatch: Colors.blue,               
+      ),
+      home: Employee(),
+      debugShowCheckedModeBanner: false,
+    ),
+    );
+  }
+}
+
+  
+
